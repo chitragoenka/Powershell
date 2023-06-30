b5e35de170f8c394271bcdd19b6958745fbb1666
chitragoenka <chitra.goenka@gmail.com>
2023-06-30 11:50:53 -0400

    Basics

---
+++ test.ps1
@@ -0,0 +1,15 @@
+#Basic fundamentals of Powershell
+#Source- https://www.youtube.com/watch?v=NECE5CX69tk
+
+#Getting help with list of commands
+
+Get-Service
+Get-Date
+
+#gets all the command-lets that have the noun service
+Get-Command -noun service
+
+
+gsv #short hand for get-service
+
+Get-Alias
